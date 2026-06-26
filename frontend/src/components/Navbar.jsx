b8ce32947d91d398440c34217ccc733828ff4485
alamin20cse <md.alamin165086@gmail.com>
2026-06-27 02:08:32 +0600

    profile loading remove

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
       const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 
-    if (isLoadingPfofile) return <Loading></Loading>
+    if (isLoadingPfofile) return <div>loading profile </div>
     // console.log(profile);
 
   // console.log(incompleteCarts.length);
